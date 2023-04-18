2ed3072ebdf890287cb7f4a76421024fa72a9e19
Crowdbotics <team@crowdbotics.com>
2023-04-18 20:22:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cool-credit-40017",
+  url: "https://cool-credit-40017.botics.co",
+  api: "https://cool-credit-40017.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
